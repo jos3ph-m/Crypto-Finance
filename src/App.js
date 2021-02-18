36ac107605d88e2a30e1bb427a86b3fe127fd054
Joseph M <jmedina1985@hotmail.com>
2021-02-18 11:54:58 -0800

    Axios included

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+
+let url =
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
 function App() {
   return (
