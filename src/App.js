7a9ca125cbece2a67447b8a053098a09dba6ed51
Joseph M <jmedina1985@hotmail.com>
2021-02-18 11:59:52 -0800

    successfully returning data from api

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-let url =
-  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
-
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .then((res) => {
+        setCoins(res.data);
+      });
+  });
   return (
     <div className="App">
       <h1>Crypto api</h1>
