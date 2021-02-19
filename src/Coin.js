75538f6f4a2184621917bae81ed4d5bd8ffe928e
Joseph M <jmedina1985@hotmail.com>
2021-02-18 22:59:21 -0800

    building Coin.js - 3

--- src/Coin.js
+++ src/Coin.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Coin = () => {
-  return <div className="coin-container"></div>;
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Coin;
