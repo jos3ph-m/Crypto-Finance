7dee88244e6bad2760b5d149a81ffcf296ac2eac
Joseph M <jmedina1985@hotmail.com>
2021-02-20 21:29:51 -0800

    building Coin.js - 23

--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
-const Coin = ({ name, image, symbol, price, volume, priceChange }) => {
+const Coin = ({
+  name,
+  image,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketcap,
+}) => {
   return (
     <div className="coin-container">
       <div className="coin-row">
@@ -17,6 +25,9 @@ const Coin = ({ name, image, symbol, price, volume, priceChange }) => {
           ) : (
             <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
           )}
+          <p className="coin-marketcap">
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
